668cacfaa1d928c7d48e96dca2b52a9f02cec76c
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-07-11 16:30:45 +1000

    unit of work

---
+++ Backend/Emp.Core/RepositoryInterfaces/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Emp.Core.RepositoryInterfaces;
+
+public interface IUnitOfWork : IDisposable
+{
+    IEmployeeRepository EmployeeRepository { get; }
+    IDepartmentRepository DepartmentRepository { get; }
+    Task<int> CompleteAsync();
+}

a57dd33296325822a6d34defcfa2844680e562d4
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-07-11 16:08:29 +1000

    entities added

--- Backend/Emp.Core/Entities/Department.cs
+++ Backend/Emp.Core/Entities/Department.cs
@@ -1,14 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Emp.Core.Entities;
-
-
-public class BaseEntity
-{
-    [Key]
-    public int Id { get; set; }
-}
 
 public class Department : BaseEntity
 {
@@ -16,27 +8,4 @@ public class Department : BaseEntity
     [MaxLength(100)]
     public required string Name { get; set; }
     public virtual ICollection<Employee> Employees { get; set; } = [];
-}
-
-public class Employee : BaseEntity
-{
-    [Required]
-    [MaxLength(100)]
-    public required string FirstName { get; set; }
-
-    [Required]
-    [MaxLength(100)]
-    public required string LastName { get; set; }
-
-    [Required]
-    [EmailAddress]
-    [MaxLength(100)]
-    public required string EmailAddress { get; set; }
-
-    [Required]
-    [ForeignKey("DepartmentId")]
-    public int DepartmentId { get; set; }
-
-    public virtual required Department Department { get; set; }
-
 }
---
+++ Backend/Emp.Core/Entities/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Emp.Core.Entities;
+
+public class Employee : BaseEntity
+{
+    [Required]
+    [MaxLength(100)]
+    public required string FirstName { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public required string LastName { get; set; }
+
+    [Required]
+    [EmailAddress]
+    [MaxLength(100)]
+    public required string EmailAddress { get; set; }
+
+    [Required]
+    [ForeignKey("DepartmentId")]
+    public int DepartmentId { get; set; }
+
+    public virtual required Department Department { get; set; }
+}

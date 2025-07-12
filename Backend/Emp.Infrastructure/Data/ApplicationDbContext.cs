827821efd56ab7f43779e0090abe35244aeaaa2f
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-07-12 15:43:26 +1000

    GR

---
+++ Backend/Emp.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Emp.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Emp.Infrastructure.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Department> Departments { get; set; }
+    public DbSet<Employee> Employees { get; set; }
+
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Employee>()
+            .HasOne(e => e.Department)
+            .WithMany(d => d.Employees)
+            .HasForeignKey(e => e.DepartmentId)
+            .OnDelete(DeleteBehavior.Restrict);  // deletion of department will throw an error, if it still has atleast one employee
+    }
+}
---
+++ Backend/Emp.Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,49 @@
+﻿using Emp.Core.Entities;
+using Emp.Core.Interfaces.Repositories;
+using Emp.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Emp.Infrastructure.Repositories;
+
+public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+{
+    private readonly ApplicationDbContext context;
+    private readonly DbSet<T> dbSet;
+
+    public GenericRepository(ApplicationDbContext context)
+    {
+        this.context = context;
+        dbSet = context.Set<T>();
+    }
+
+    public async Task<IEnumerable<T>> GetAllAsync()
+    {
+        return await dbSet.AsNoTracking().ToListAsync();        // not for editing - as no tracking
+    }
+
+    public async Task<T?> GetByIdAsync(int id)
+    {
+        return await dbSet.FindAsync(id);
+    }
+
+    public async Task AddAsync(T entity)
+    {
+        await dbSet.AddAsync(entity);
+    }
+
+    public void Update(T entity)
+    {
+        dbSet.Attach(entity);
+        context.Entry(entity).State = EntityState.Modified;
+    }
+
+    public void Delete(T entity)
+    {
+        context.Remove(entity);
+    }
+
+    public async Task<IEnumerable<T>> FindAsync(System.Linq.Expressions.Expression<Func<T, bool>> predicate)
+    {
+        return await dbSet.Where(predicate).ToListAsync();
+    }
+}

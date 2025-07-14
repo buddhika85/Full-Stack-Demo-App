d5a3d05302987ea3fc6820444d439f51ea8df683
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-07-14 16:23:58 +1000

    employee controller - get all

---
+++ Backend/Emp.Api/Controllers/EmployeeController.cs
@@ -0,0 +1,48 @@
+﻿using Emp.Core.DTOs;
+using Emp.Core.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OutputCaching;
+
+namespace Emp.Api.Controllers;
+
+public class EmployeeController : BaseController
+{
+    private readonly IEmployeeService employeeService;
+    private readonly ILogger<EmployeeController> logger;
+    private readonly IOutputCacheStore outputCacheStore;
+    private const string cacheTag = "employees";
+
+    public EmployeeController(IEmployeeService employeeService, ILogger<EmployeeController> logger, IOutputCacheStore outputCacheStore)
+    {
+        this.employeeService = employeeService;
+        this.logger = logger;
+        this.outputCacheStore = outputCacheStore;
+    }
+
+    [EndpointSummary("Gets all employees")]
+    [ProducesResponseType(typeof(IEnumerable<EmployeeDto>), StatusCodes.Status200OK)]
+    [HttpGet]
+    [OutputCache(Tags = [cacheTag])]
+    public async Task<ActionResult<IEnumerable<EmployeeDto>>> GetEmployees()
+    {
+        try
+        {
+            return Ok(await employeeService.GetAllEmployeesAsync());
+        }
+        catch (Exception ex)
+        {
+            const string error = "Error occured while retreiving employees";
+            logger.LogError(ex, error);
+            return InternalServerError(error);
+        }
+    }
+
+
+    // GetEmployee
+
+    // CreateEmployees
+
+    // UpdateEmployees
+
+    // DeleteEmployees
+}

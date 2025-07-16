01a3793519fbdbc1b046d914d3f390e0039ea355
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-07-16 17:30:22 +1000

    documentation

--- Backend/Emp.Api/Controllers/EmployeeController.cs
+++ Backend/Emp.Api/Controllers/EmployeeController.cs
@@ -1,6 +1,8 @@
 ﻿
 using Emp.Api.Controllers;
+using Emp.Core.DTOs;
 using Emp.Core.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.OutputCaching;
 
 namespace Emp.Api.Middleware;
@@ -18,5 +20,128 @@ public class EmployeeController : BaseController
         this.employeeService = employeeService;
         this.logger = logger;
         this.outputCacheStore = outputCacheStore;
+    }
+
+    [EndpointSummary("Gets all employees")]
+    [ProducesResponseType(typeof(IEnumerable<EmployeeDto>), StatusCodes.Status200OK)]
+    [HttpGet]
+    [OutputCache(Tags = [cacheTag])]
+    public async Task<ActionResult<IEnumerable<EmployeeDto>>> GetEmployees()
+    {
+        try
+        {
+            return Ok(await employeeService.GetAllEmployeesAsync());
+        }
+        catch (Exception ex)
+        {
+            const string error = "Error occured while retreiving employees";
+            logger.LogError(ex, error);
+            return InternalServerError(error);
+        }
+    }
+
+    [EndpointSummary("Get employee by ID")]
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<EmployeeDto>> GetEmployee([FromRoute] int id)
+    {
+        try
+        {
+            var employee = await employeeService.GetEmployeeByIdAsync(id);
+            if (employee == null)
+            {
+                logger.LogWarning("Employee with ID {id} does not exist.", id);
+                return NotFoundError($"Employee with ID {id} does not exist.");
+            }
+            return Ok(employee);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Error finding employee with ID {id}.", id);
+            return InternalServerError($"Error finding employee with ID {id}.");
+        }
+    }
+
+    [EndpointSummary("Create a new employee")]
+    [HttpPost]
+    public async Task<ActionResult<EmployeeDto>> CreateEmployee([FromBody] CreateEmployeeDto createEmployeeDto)
+    {
+        try
+        {
+            var employee = await employeeService.CreateEmployeeAsync(createEmployeeDto);
+            if (employee == null)
+            {
+                logger.LogError("Error creating an employee with email {email}.", createEmployeeDto.Email);
+                return InternalServerError($"Error creating an employee with email {createEmployeeDto.Email}.");
+            }
+            await outputCacheStore.EvictByTagAsync(cacheTag, default);
+            return CreatedAtAction(nameof(GetEmployee), new { id = employee.Id }, employee);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Error creating an employee with email {email}.", createEmployeeDto.Email);
+            return InternalServerError($"Error creating an employee with email {createEmployeeDto.Email}.");
+        }
+    }
+
+    [EndpointSummary("Updates an employee")]
+    [HttpPut("{id:int}")]
+    public async Task<IActionResult> UpdateEmployee([FromRoute] int id, [FromBody] UpdateEmployeeDto updateEmployeeDto)
+    {
+        try
+        {
+            if (id != updateEmployeeDto.Id)
+            {
+                logger.LogError("Unable to update. Employee Id passed from route {id} does not match with body employee Id {dtoId}", id, updateEmployeeDto.Id);
+                return ValidationError("id", $"Unable to update. Employee Id passed from route {id} does not match with body employee Id {updateEmployeeDto.Id}");
+            }
+
+            if (await employeeService.GetEmployeeByIdAsync(id) == null)
+            {
+                logger.LogError("Unable to update. Employee with ID {id} does not exist.", id);
+                return NotFoundError($"Unable to update. Employee with ID {id} does not exist.");
+            }
+
+            if (!await employeeService.UpdateEmployeeAsync(id, updateEmployeeDto))
+            {
+                logger.LogError("Error occured while updating employee with ID {id} and email {email}", id, updateEmployeeDto.Email);
+                return InternalServerError($"Error occured while updating employee with ID {id} and email {updateEmployeeDto.Email}");
+            }
+
+            await outputCacheStore.EvictByTagAsync(cacheTag, default);
+            return NoContent();
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Error in updating employee with ID {id} and email {email}", id, updateEmployeeDto.Email);
+            return InternalServerError($"Error in updating employee with ID {id} and email {updateEmployeeDto.Email}");
+        }
+    }
+
+    [EndpointSummary("Deletes an employee")]
+    [HttpDelete("{id:int}")]
+    public async Task<IActionResult> DeleteEmployees(int id)
+    {
+        try
+        {
+            if (await employeeService.GetEmployeeByIdAsync(id) == null)
+            {
+                logger.LogError("Unable to delete. Employee with ID {id} does not exist.", id);
+                return NotFoundError($"Unable to delete. Employee with ID {id} does not exist.");
+            }
+
+            if (!await employeeService.DeleteEmployeeAsync(id))
+            {
+                logger.LogError("Error occured while deleting employee with ID {id}", id);
+                return InternalServerError($"Error occured while deleting employee with ID {id}");
+            }
+
+            await outputCacheStore.EvictByTagAsync(cacheTag, default);
+            return NoContent();
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Error in deleting employee with ID {id}}", id);
+            return InternalServerError($"Error in deleting employee with ID {id}");
+        }
     }
 }
---
+++ Backend/Emp.XUnitTests/Services/DepartmentServiceTests.cs
@@ -0,0 +1,52 @@
+﻿using Emp.Application.Services;
+using Emp.Core;
+using Emp.Core.Entities;
+using Emp.Core.Interfaces.Repositories;
+using Emp.Core.Interfaces.Services;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace Emp.XUnitTests.Services;
+
+public class DepartmentServiceTests
+{
+    private readonly Mock<IDepartmentRepository> mockDepartmentRepository;
+    private readonly Mock<IEmployeeRepository> mockEmployeeRepository;
+    private readonly Mock<IUnitOfWork> mockUnitOfWork;
+
+    private readonly Mock<ILogger<DepartmentService>> mockLogger;
+    private readonly IDepartmentService departmentService;
+
+    public DepartmentServiceTests()
+    {
+        mockDepartmentRepository = new Mock<IDepartmentRepository>();
+        mockEmployeeRepository = new Mock<IEmployeeRepository>();
+
+        mockUnitOfWork = new Mock<IUnitOfWork>();
+        mockUnitOfWork.Setup(x => x.DepartmentRepository).Returns(mockDepartmentRepository.Object);
+        mockUnitOfWork.Setup(x => x.EmployeeRepository).Returns(mockEmployeeRepository.Object);
+
+        mockLogger = new Mock<ILogger<DepartmentService>>();
+        departmentService = new DepartmentService(mockUnitOfWork.Object, mockLogger.Object);
+    }
+
+    [Fact]
+    public async Task GetAllDepartmentsAsync_GetsAllDepartments_WhenCalled()
+    {
+        // arrange
+        var departments = new List<Department>
+            {
+                new Department { Id = 1, Name = "HR" },
+                new Department { Id = 2, Name = "IT" }
+            };
+        mockDepartmentRepository.Setup(x => x.GetAllAsync()).ReturnsAsync(departments);
+
+        // act
+        var result = await departmentService.GetAllDepartmentsAsync();
+
+        // assert
+        result.Should().NotBeNull();
+        result.Should().HaveCount(departments.Count());
+    }
+}

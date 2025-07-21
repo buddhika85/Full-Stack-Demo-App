c38965cc83aff0b39053269b0bc1001b0f5d3412
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-07-21 16:06:49 +1000

    API - user entity added

---
+++ Backend/Emp.Core/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace Emp.Core.Entities;
+
+public class User : BaseEntity
+{
+    [Required]
+    [MaxLength(50)]
+    public required string Username { get; set; }
+
+    [Required]
+    public required string PasswordHash { get; set; }
+
+    [Required]
+    [MaxLength(20)]
+    public required string Role { get; set; }
+
+    public bool IsActive { get; set; } = true;
+}

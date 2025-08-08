0adfff22aa17a61e68f519bd6a8807ac7324050c
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-08-08 14:29:50 +1000

    Ang - login form created

--- Emp.Angular/src/app/components/login/login.ts
+++ Emp.Angular/src/app/components/login/login.ts
@@ -1,11 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { LoginDto } from '../../models/login.dto';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
-  styleUrl: './login.scss'
+  styleUrl: './login.scss',
 })
-export class Login {
+export class Login implements OnInit {
+  private loginDto: LoginDto = { username: '', password: '' };
+  private readonly formBuilder: FormBuilder = inject(FormBuilder);
+  formGroup!: FormGroup<{
+    username: FormControl<string>;
+    password: FormControl<string>;
+  }>;
 
+  ngOnInit(): void {
+    this.formGroup = this.formBuilder.group({
+      username: new FormControl(this.loginDto.username, {
+        nonNullable: true,
+        validators: [Validators.required, Validators.minLength(5)],
+      }),
+      password: new FormControl(this.loginDto.password, {
+        nonNullable: true,
+        validators: [Validators.required, Validators.minLength(5)],
+      }),
+    });
+  }
+
+  get username(): FormControl<string> {
+    return this.formGroup.controls.username;
+  }
+
+  get password(): FormControl<string> {
+    return this.formGroup.controls.password;
+  }
+
+  onSubmit(): void {
+    if (this.formGroup.valid) {
+      this.loginDto = this.formGroup.getRawValue();
+      console.log(this.loginDto);
+    }
+  }
 }

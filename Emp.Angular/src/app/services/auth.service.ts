88e28e682dea61e0069a3c7513674cf106ff0790
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-08-09 09:50:00 +1000

    Ang - token service

--- Emp.Angular/src/app/services/auth.service.ts
+++ Emp.Angular/src/app/services/auth.service.ts
@@ -5,64 +5,35 @@ import { Observable, tap, catchError, of } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { LoginDto } from '../models/login.dto';
 import { UserDto } from '../models/user.dto';
-import { jwtDecode } from 'jwt-decode';
+import { JwtTokenService } from './jwt.token.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  private httpClient: HttpClient = inject(HttpClient);
-  private baseUrl: string = `${environment.apiUrl}`;
+  private readonly httpClient: HttpClient = inject(HttpClient);
+  private readonly baseUrl: string = `${environment.apiUrl}`;
+
+  private readonly jwtTokenService: JwtTokenService = inject(JwtTokenService);
 
   currentUser = signal<UserDto | null>(null);
 
+  // on refresh of pages - auth service will decode token if exists
   constructor() {
-    const token = localStorage.getItem('jwtToken');
+    const token = this.jwtTokenService.readJwtAuthToken();
 
-    if (token && !this.isTokenExpired(token)) {
-      let user: UserDto | null = this.decodeToken(token);
-      if (user) {
-        this.currentUser.set(user);
+    if (token) {
+      if (this.jwtTokenService.isTokenExpired(token)) {
+        console.log('Token expired - so remove from local storage');
+        this.jwtTokenService.removeToken();
       } else {
-        this.removeToken();
+        let user: UserDto | null = this.jwtTokenService.decodeToken(token);
+        if (user) {
+          this.currentUser.set(user);
+        } else {
+          this.jwtTokenService.removeToken();
+        }
       }
-    } else {
-      console.log('Token expired - so remove from local storage');
-      this.removeToken();
-    }
-  }
-
-  // extract claims from JWT token
-  private decodeToken(token: string): UserDto | null {
-    try {
-      const decodedToken: any = jwtDecode(token);
-      let user: UserDto = {
-        id: parseInt(decodedToken.nameid),
-        username: decodedToken.unique_name,
-        firstName: decodedToken.given_name || '',
-        lastName: decodedToken.family_name || '',
-        role: decodedToken.role,
-        isActive: decodedToken.is_active === true,
-      };
-      return user;
-    } catch (e) {
-      console.error('Error decoding token:', e);
-      return null;
-    }
-  }
-
-  private isTokenExpired(token: string): boolean {
-    try {
-      const decoded: any = jwtDecode(token);
-      const exp = decoded.exp;
-
-      if (!exp) return true; // No expiration claim — treat as expired
-
-      const currentTime = Math.floor(Date.now() / 1000); // Current time in seconds
-      return exp < currentTime; // true if expired
-    } catch (e) {
-      console.error('Error decoding token:', e);
-      return true; // If decoding fails, treat as expired
     }
   }
 
@@ -71,23 +42,23 @@ export class AuthService {
       .post<LoginResponseDto>(`${this.baseUrl}/auth/login`, loginDto)
       .pipe(
         tap((response) => {
-          localStorage.setItem('jwtToken', response.token);
+          this.jwtTokenService.setJwtToken(response.token);
           this.currentUser.set(response.user);
         }),
         catchError((error) => {
           console.error('Login failed:', error);
-          this.removeToken();
+          this.cleanTokenUser();
           return of(error);
         })
       );
   }
 
-  removeToken(): void {
-    localStorage.removeItem('jwtToken');
+  cleanTokenUser(): void {
+    this.jwtTokenService.removeToken();
     this.currentUser.set(null);
   }
 
   logout(): void {
-    this.removeToken();
+    this.cleanTokenUser();
   }
 }
---
+++ Emp.Angular/src/app/services/jwt.token.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+import { UserDto } from '../models/user.dto';
+import { jwtDecode } from 'jwt-decode';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class JwtTokenService {
+  // local storage key for jwt auth token
+  private readonly jwtTokenKey: string = 'jwtToken';
+
+  setJwtToken(token: string): void {
+    localStorage.setItem(this.jwtTokenKey, token);
+  }
+
+  readJwtAuthToken(): string | null {
+    return localStorage.getItem(this.jwtTokenKey);
+  }
+
+  removeToken(): void {
+    localStorage.removeItem(this.jwtTokenKey);
+  }
+
+  // extract claims from JWT token
+  decodeToken(token: string): UserDto | null {
+    try {
+      const decodedToken: any = jwtDecode(token);
+      let user: UserDto = {
+        id: parseInt(decodedToken.nameid),
+        username: decodedToken.unique_name,
+        firstName: decodedToken.given_name || '',
+        lastName: decodedToken.family_name || '',
+        role: decodedToken.role,
+        isActive: decodedToken.is_active === true,
+      };
+      return user;
+    } catch (e) {
+      console.error('Error decoding token:', e);
+      return null;
+    }
+  }
+
+  isTokenExpired(token: string): boolean {
+    try {
+      const decoded: any = jwtDecode(token);
+      const exp = decoded.exp;
+
+      if (!exp) return true; // No expiration claim — treat as expired
+
+      const currentTime = Math.floor(Date.now() / 1000); // Current time in seconds
+      return exp < currentTime; // true if expired
+    } catch (e) {
+      console.error('Error decoding token:', e);
+      return true; // If decoding fails, treat as expired
+    }
+  }
+}

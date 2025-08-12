0482a625df554e768dc2f8c6b68c11ee3ca80e9e
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-08-12 16:53:38 +1000

    Ang - get all users

--- Emp.Angular/src/app/components/auth/admin/manage-app-users/manage-app-users.ts
+++ Emp.Angular/src/app/components/auth/admin/manage-app-users/manage-app-users.ts
@@ -1,14 +1,46 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  inject,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatSort, MatSortModule } from '@angular/material/sort';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { UserService } from '../../../../services/user.service';
+import { Subscription } from 'rxjs';
+import { UserDto } from '../../../../models/user.dto';
 
 @Component({
   selector: 'app-manage-app-users',
   imports: [],
+
   templateUrl: './manage-app-users.html',
   styleUrl: './manage-app-users.scss',
 })
-export class ManageAppUsers {}
+export class ManageAppUsers implements OnInit, OnDestroy {
+  private readonly userService: UserService = inject(UserService);
+  private readonly compositeSubscription: Subscription = new Subscription();
+
+  private users!: UserDto[];
+
+  ngOnInit(): void {
+    const sub = this.userService.getUsers().subscribe({
+      next: (users: UserDto[]) => {
+        this.users = users;
+        console.log(users);
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.compositeSubscription.unsubscribe();
+  }
+}

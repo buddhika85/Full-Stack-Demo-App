9c5bf8f5e148142c150887af12804a9f68bf603b
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-08-13 14:16:12 +1000

    Ang - users form and create, edit users components added

---
+++ Emp.Angular/src/app/components/auth/admin/manage-app-users/create-users/create-users.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-users',
+  imports: [],
+  templateUrl: './create-users.html',
+  styleUrl: './create-users.scss'
+})
+export class CreateUsers {
+
+}
---
+++ Emp.Angular/src/app/components/auth/admin/manage-app-users/edit-users/edit-users.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-users',
+  imports: [],
+  templateUrl: './edit-users.html',
+  styleUrl: './edit-users.scss'
+})
+export class EditUsers {
+
+}

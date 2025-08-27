c046307417e11387480df01e2f710d58e4d06f08
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-08-27 18:52:27 +1000

    API - added IPasswordHasher

---
+++ Backend/Emp.Core/Interfaces/Services/IPasswordHasher.cs
@@ -0,0 +1,7 @@
+﻿namespace Emp.Core.Interfaces.Services;
+
+public interface IPasswordHasher
+{
+    public bool VerifyPassword(string password, string hashedPassword);
+    public string HashPassword(string password);
+}

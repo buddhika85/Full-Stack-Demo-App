6cb463b423ce3c55050aa4f53ccb7afdb6ac114a
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-08-27 15:52:17 +1000

    X-Unit - CreateUser_ReturnsBadRequestResult_WhenEmailIsInvalid

--- Backend/Emp.XUnitTests/Helpers/Helpers.cs
+++ Backend/Emp.XUnitTests/Helpers/Helpers.cs
@@ -1,4 +1,9 @@
-﻿namespace Emp.XUnitTests.Helpers;
+﻿using Emp.Api.Controllers;
+using Emp.Core.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Emp.XUnitTests.Helpers;
 
 public static class Helpers
 {
@@ -6,5 +11,15 @@ public static class Helpers
     {
         return value?.ToString() is string message
             && message.Contains(expectedLogMessage);
+    }
+
+    public static void ApplyModelStateErrors(CreateUserDto dto, BaseController controller)
+    {
+        var context = new ValidationContext(dto);
+        var results = new List<ValidationResult>();
+        Validator.TryValidateObject(dto, context, results, true);
+
+        foreach (var error in results)
+            controller.ModelState.AddModelError(error.MemberNames.First(), error.ErrorMessage);
     }
 }

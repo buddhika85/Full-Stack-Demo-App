d00f3581d38f959ffaa55b6b21461edd016950d8
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-08-30 18:34:41 +1000

    Ang - department components added

---
+++ Emp.Angular/src/app/components/auth/manage-departments/create-departments/create-departments.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-departments',
+  imports: [],
+  templateUrl: './create-departments.html',
+  styleUrl: './create-departments.scss'
+})
+export class CreateDepartments {
+
+}

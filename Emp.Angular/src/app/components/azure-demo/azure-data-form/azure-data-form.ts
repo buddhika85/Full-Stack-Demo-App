880361e2bc3e8b895fd7d62e57dd47f5c6ee9e41
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-09-03 12:28:18 +1000

    Ang - Az Page Form component added

---
+++ Emp.Angular/src/app/components/azure-demo/azure-data-form/azure-data-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-azure-data-form',
+  imports: [],
+  templateUrl: './azure-data-form.html',
+  styleUrl: './azure-data-form.scss'
+})
+export class AzureDataForm {
+
+}
--- Emp.Angular/src/app/components/azure-demo/azure-demo.ts
+++ Emp.Angular/src/app/components/azure-demo/azure-demo.ts
@@ -1,9 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { AzureService } from '../../services/azure.service';
+import { AzureDataForm } from './azure-data-form/azure-data-form';
 
 @Component({
   selector: 'app-azure-demo',
-  imports: [],
+  imports: [AzureDataForm],
   templateUrl: './azure-demo.html',
   styleUrl: './azure-demo.scss',
 })

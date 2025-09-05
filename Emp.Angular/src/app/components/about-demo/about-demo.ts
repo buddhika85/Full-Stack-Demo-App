a0850885506c10f921dfbc6b1f2c32df7be7bc4c
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-09-05 10:26:23 +1000

    Ang - added Az Dev Ops Badge to footer and other pages

--- Emp.Angular/src/app/components/about-demo/about-demo.ts
+++ Emp.Angular/src/app/components/about-demo/about-demo.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-about-demo',
   imports: [],
   templateUrl: './about-demo.html',
-  styleUrl: './about-demo.scss'
+  styleUrl: './about-demo.scss',
 })
 export class AboutDemo {
-
+  currentTime = signal<string>(
+    new Date().toLocaleString('en-AU', {
+      timeZone: 'Australia/Sydney',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      hour12: false,
+    })
+  );
 }

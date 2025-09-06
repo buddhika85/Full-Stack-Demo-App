16511277fd849ecacb15911a76f2f4537ce90fde
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-09-06 10:09:05 +1000

    API - Returns Allowed Origins By API

--- Backend/Emp.Api/Controllers/HomeController.cs
+++ Backend/Emp.Api/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Emp.Core.DTOs;
+using Emp.Core.Enums;
 using Emp.Core.Interfaces.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -30,7 +31,7 @@ public class HomeController : BaseController
         {
             LandingDto landingDto = new();
             landingDto.Departments = await departmentService.GetAllDepartmentsWithEmpCountsAsync();
-            logger.Log(LogLevel.Information, "API: Landing page endpoint called.");
+            logger.LogInformation("API: Landing page endpoint called.");
             return Ok(landingDto);
         }
         catch (Exception ex)
@@ -40,4 +41,44 @@ public class HomeController : BaseController
             return InternalServerError(error);
         }
     }
+
+    [EndpointSummary("Returns Allowed Origins By Backend API")]
+    //[Authorize(Roles = $"{nameof(UserRoles.Admin)},{nameof(UserRoles.Staff)}")]
+    [HttpGet]
+    public ActionResult<IEnumerable<string>> GetAllowedOriginList(IConfiguration configuration)
+    {
+        try
+        {
+            logger.LogInformation("API: Get Allowed Origin List endpoint called.");
+            var strAllowedOrigins = configuration["AllowedOrigins"]?.ToString();
+            if (string.IsNullOrWhiteSpace(strAllowedOrigins))
+            {
+                const string error = "AllowedOrigins - setting is not available";
+                throw new ArgumentException(error);
+            }
+            var allowedOriginList = strAllowedOrigins.Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+            if (allowedOriginList == null || allowedOriginList.Count() == 0)
+            {
+                const string error = "AllowedOrigins - is not set - but setting is available";
+                throw new ArgumentException(error);
+            }
+
+
+            logger.LogInformation("Raw AllowedOrigins string: {origins}", strAllowedOrigins);
+            logger.LogInformation("Parsed AllowedOrigins list: {@list}", allowedOriginList);
+            return Ok(allowedOriginList);
+        }
+        catch (ArgumentException ex)
+        {
+            logger.LogError(ex, ex.Message);
+            return InternalServerError($"Internal Server Error occurred while reading Allowed-Orgin for CORS - {ex.Message}");
+        }
+        catch (Exception ex)
+        {
+            const string error = "Error occurred while reading Allowed-Orgin for CORS";
+            logger.LogError(ex, error);
+            return InternalServerError(error);
+        }
+    }
+
 }

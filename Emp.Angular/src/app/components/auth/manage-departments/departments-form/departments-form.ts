7f91265bcfc5835d3222195df949f99c76c1a8de
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-09-10 12:56:52 +1000

    Ang - Form TS code added

--- Emp.Angular/src/app/components/auth/manage-departments/departments-form/departments-form.ts
+++ Emp.Angular/src/app/components/auth/manage-departments/departments-form/departments-form.ts
@@ -4,10 +4,19 @@ import {
   Input,
   OnChanges,
   OnDestroy,
+  OnInit,
   SimpleChanges,
 } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { DepartmentService } from '../../../../services/department.service';
+import { DepartmentDto } from '../../../../models/department.dto';
+import { SnackbarService } from '../../../../services/snackbar.service';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-departments-form',
@@ -15,15 +24,26 @@ import { DepartmentService } from '../../../../services/department.service';
   templateUrl: './departments-form.html',
   styleUrl: './departments-form.scss',
 })
-export class DepartmentsForm implements OnChanges, OnDestroy {
+export class DepartmentsForm implements OnInit, OnChanges, OnDestroy {
   private readonly compositeSubscription: Subscription = new Subscription();
+  private readonly snackBarService: SnackbarService = inject(SnackbarService);
   private readonly departmentService: DepartmentService =
     inject(DepartmentService);
   @Input() departmentId: number | null = null;
 
+  private departmentModel: DepartmentDto = { id: 0, name: '' };
+  private readonly formBuilder: FormBuilder = new FormBuilder();
+  formGroup!: FormGroup<{
+    name: FormControl<string>;
+  }>;
+
+  ngOnInit(): void {
+    this.buildForm();
+  }
+
   ngOnChanges(changes: SimpleChanges): void {
     if (this.departmentId) {
-      this.prepareFormForEditMode();
+      this.prepareFormForEditMode(this.departmentId);
     }
   }
 
@@ -31,7 +51,36 @@ export class DepartmentsForm implements OnChanges, OnDestroy {
     this.compositeSubscription.unsubscribe();
   }
 
-  prepareFormForEditMode() {
-    throw new Error('Method not implemented.');
+  private buildForm(): void {
+    this.formGroup = this.formBuilder.group({
+      name: new FormControl<string>(this.departmentModel.name, {
+        nonNullable: true,
+        validators: [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(50),
+        ],
+      }),
+    });
+  }
+
+  private prepareFormForEditMode(editId: number) {
+    const sub = this.departmentService.getDepartmentById(editId).subscribe({
+      next: (value: DepartmentDto) => {
+        this.departmentModel = { ...value };
+      },
+      error: (error: any) => {
+        //console.error('Error - ', error);
+        if (error.error && error.error.detail) {
+          this.snackBarService.error(`${error.error.detail}`);
+          return;
+        }
+
+        this.snackBarService.error(
+          `Error occured while retrieving department with ID ${editId}`
+        );
+      },
+    });
+    this.compositeSubscription.add(sub);
   }
 }

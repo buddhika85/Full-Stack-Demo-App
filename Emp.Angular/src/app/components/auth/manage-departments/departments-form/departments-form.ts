9cefcffc23a5dcaafb9daa9a8497944bcd8e7144
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-09-10 12:13:40 +1000

    Ang - department service methods added

--- Emp.Angular/src/app/components/auth/manage-departments/departments-form/departments-form.ts
+++ Emp.Angular/src/app/components/auth/manage-departments/departments-form/departments-form.ts
@@ -1,4 +1,13 @@
-import { Component, Input } from '@angular/core';
+import {
+  Component,
+  inject,
+  Input,
+  OnChanges,
+  OnDestroy,
+  SimpleChanges,
+} from '@angular/core';
+import { Subscription } from 'rxjs';
+import { DepartmentService } from '../../../../services/department.service';
 
 @Component({
   selector: 'app-departments-form',
@@ -6,6 +15,23 @@ import { Component, Input } from '@angular/core';
   templateUrl: './departments-form.html',
   styleUrl: './departments-form.scss',
 })
-export class DepartmentsForm {
+export class DepartmentsForm implements OnChanges, OnDestroy {
+  private readonly compositeSubscription: Subscription = new Subscription();
+  private readonly departmentService: DepartmentService =
+    inject(DepartmentService);
   @Input() departmentId: number | null = null;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this.departmentId) {
+      this.prepareFormForEditMode();
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.compositeSubscription.unsubscribe();
+  }
+
+  prepareFormForEditMode() {
+    throw new Error('Method not implemented.');
+  }
 }

84c914e7fa5508792d42a5d2471918660fc44d25
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-09-19 10:25:37 +1000

    GetByIdAsync_ReturnsEmployeeById_WhenEmployeeAvailableById

--- Backend/Emp.XUnitTests/TestData/EmployeeTestData.cs
+++ Backend/Emp.XUnitTests/TestData/EmployeeTestData.cs
@@ -1,5 +1,14 @@
-﻿namespace Emp.XUnitTests.TestData;
+﻿
+using Emp.Core.Entities;
 
-public class EmployeeTestData //: BaseTestData
+namespace Emp.XUnitTests.TestData;
+
+public class EmployeeTestData : BaseTestData
 {
+    public override IEnumerator<object[]> GetEnumerator()
+    {
+        yield return new object[] { new Employee { FirstName = "John", LastName = "Doe", Email = "john.doe@example.com", DepartmentId = 2 } };
+        yield return new object[] { new Employee { FirstName = "Jane", LastName = "Smith", Email = "jane.smith@example.com", DepartmentId = 1 } };
+        yield return new object[] { new Employee { FirstName = "Peter", LastName = "Jones", Email = "peter.jones@example.com", DepartmentId = 2 } };
+    }
 }
